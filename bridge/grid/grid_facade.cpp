b4bdcd875c3c9336c708b77d6e534a123fe5c69f
Malinovskii Vladimir <galqiwi@galqiwi.ru>
2021-12-31 16:09:17 +0300

    new facade and stuff

---
+++ bridge/grid/grid_facade.cpp
@@ -0,0 +1,5 @@
+//
+// Created by galqiwi on 30.12.2021.
+//
+
+#include "TGridFacade.h"
---
+++ bridge/grid/grid_facade.h
@@ -0,0 +1,10 @@
+//
+// Created by galqiwi on 30.12.2021.
+//
+
+#ifndef RULE30_21_TGRIDFACADE_H
+#define RULE30_21_TGRIDFACADE_H
+
+class TGridFacade {};
+
+#endif  // RULE30_21_TGRIDFACADE_H
